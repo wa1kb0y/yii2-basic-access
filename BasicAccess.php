61de1d99d9f43c0e6546ed10b07a7fe4662f3261
Vitaly Walkboy <walkboy@mail.ru>
2020-12-06 23:08:42 +0300

    init

---
+++ BasicAccess.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace walkboy\BasicAccess;
+
+use yii\base\ActionFilter;
+
+class BasicAccess extends ActionFilter
+{
+	public $logins = null;
+	public $realm = 'WWW';
+	public $msg_unauthorized = 'Please enter a valid user name and password to access.';
+
+    public function beforeAction($action)
+    {
+    	if ($this->logins) {
+	    	$logins_arr = explode("\r\n", $this->logins);
+
+	    	for (; 1; $this->authenticate()) {
+
+	    	    if (!isset($_SERVER['PHP_AUTH_USER'])) {
+	    	        continue;
+	    	    }
+
+	    	    $user = $_SERVER['PHP_AUTH_USER'];
+	    	    $pwd = $_SERVER['PHP_AUTH_PW'];
+
+	    	    $tmp = preg_grep("/$user:.*$/", $logins_arr);
+	    	    if (!($authUserLine = array_shift($tmp))) {
+	    	        continue;
+	    	    }
+
+	    	    preg_match("/$user:((..).*)$/", $authUserLine, $matches);
+	    	    if (empty($matches)) {
+	    	        continue;
+                }
+	    	    $pwd_stored = $matches[1];
+	    	    
+	    	    if ($pwd != $pwd_stored) {
+	    	        continue;
+	    	    }
+
+	    	    break;
+	    	}
+	    }
+        
+		//throw new UnauthorizedHttpException('Unauthorized.');
+		   
+        return parent::beforeAction($action);
+    }
+
+    public function authenticate()
+    {
+	    header('WWW-Authenticate: Basic realm="'.$this->realm.'"');
+	    header('HTTP/1.0 401 Unauthorized');
+	    echo $this->msg_unauthorized;
+	    exit;
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,28 @@
+# Installation
+
+`composer require wa1kb0y/yii2-basic-access`
+
+
+# Using
+
+In your controller:
+
+```php
+public function behaviors()
+{
+    return [
+    	
+    	//...
+
+        'basicAccess' => [
+            'class' => \walkboy\BasicAccess\BasicAccess::class,
+            'logins' => ['user:pass'],
+            //'realm' => 'My Project',
+            //'msg_unauthorized' => 'Please enter a valid user name and password to access.',
+        ],
+        
+        //...
+
+    ];
+}
+```
